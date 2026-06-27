08aa34942a1472878cce2cf03f273f536ca3f7d4
Neykoor <cafecafirexos@gmail.com>
2026-06-27 13:35:36 -0700

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { Kamijs } from './Kamijs.js';
-import { ImageProvider } from './core/ImageProvider.js';
-import { Logger, LOG_LEVELS } from './core/Logger.js';
-import { EventBus, KAMIJS_EVENTS } from './core/EventBus.js';
-import { RateLimiter } from './core/RateLimiter.js';
-
-export { Kamijs, ImageProvider, Logger, LOG_LEVELS, EventBus, KAMIJS_EVENTS, RateLimiter };
